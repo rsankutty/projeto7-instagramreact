4de1179bff260f46c346fa3c34ec8124aa071d11
rsankutty <r.sankutty@gmail.com>
2022-11-20 18:56:36 -0300

    posts maps

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,80 +1,79 @@
-export default function Posts() {
-    return (
-      <div className="posts">
-        <div className="post">
-          <div className="topo">
-            <div className="usuario">
-              <img src="assets/img/meowed.svg" />
-              meowed
-            </div>
-            <div className="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
+const postsArr = [
+  {
+    imgUsuario: "assets/img/meowed.svg",
+    nomeUsuario: "meowed",
+    userPost: "assets/img/gato-telefone.svg",
+    imgCurtiu: "assets/img/respondeai.svg",
+    nomeCurtiu: "respondeai",
+    qtdCurtiu: "101.523",
+  },
+  {
+    imgUsuario: "assets/img/barked.svg",
+    nomeUsuario: "barked",
+    userPost: "assets/img/dog.svg",
+    imgCurtiu: "assets/img/adorable_animals.svg",
+    nomeCurtiu: "adorable_animals",
+    qtdCurtiu: "99.159",
+  },
+];
+
+function Post(props) {
+  return (
+    <div className="post">
+      <div className="topo">
+        <div className="usuario">
+          <img src={props.imgUsuario}/>
+          {props.nomeUsuario}
+        </div>
+        <div className="acoes">
+          <ion-icon name="ellipsis-horizontal"></ion-icon>
+        </div>
+      </div>
+
+      <div className="conteudo">
+        <img src={props.userPost} />
+      </div>
+
+      <div className="fundo">
+        <div className="acoes">
+          <div>
+            <ion-icon name="heart-outline"></ion-icon>
+            <ion-icon name="chatbubble-outline"></ion-icon>
+            <ion-icon name="paper-plane-outline"></ion-icon>
           </div>
-  
-          <div className="conteudo">
-            <img src="assets/img/gato-telefone.svg" />
-          </div>
-  
-          <div className="fundo">
-            <div className="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-  
-            <div className="curtidas">
-              <img src="assets/img/respondeai.svg" />
-              <div className="texto">
-                Curtido por <strong>respondeai</strong> e{" "}
-                <strong>outras 101.523 pessoas</strong>
-              </div>
-            </div>
+          <div>
+            <ion-icon name="bookmark-outline"></ion-icon>
           </div>
         </div>
-  
-        <div className="post">
-          <div className="topo">
-            <div className="usuario">
-              <img src="assets/img/barked.svg" />
-              barked
-            </div>
-            <div className="acoes">
-              <ion-icon name="ellipsis-horizontal"></ion-icon>
-            </div>
-          </div>
-  
-          <div className="conteudo">
-            <img src="assets/img/dog.svg" />
-          </div>
-  
-          <div className="fundo">
-            <div className="acoes">
-              <div>
-                <ion-icon name="heart-outline"></ion-icon>
-                <ion-icon name="chatbubble-outline"></ion-icon>
-                <ion-icon name="paper-plane-outline"></ion-icon>
-              </div>
-              <div>
-                <ion-icon name="bookmark-outline"></ion-icon>
-              </div>
-            </div>
-  
-            <div className="curtidas">
-              <img src="assets/img/adorable_animals.svg" />
-              <div className="texto">
-                Curtido por <strong>adorable_animals</strong> e{" "}
-                <strong>outras 99.159 pessoas</strong>
-              </div>
-            </div>
+
+        <div className="curtidas">
+          <img src={props.imgCurtiu} />
+          <div className="texto">
+            Curtido por <strong>{props.nomeCurtiu}</strong> e{" "}
+            <strong>outras {props.qtdCurtiu} pessoas</strong>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
+
+export default function Posts() {
+  return (
+    <div className="posts">
+      {postsArr.map((postItem) => {
+        return (
+          <Post
+            key={postItem.nomeUsuario}
+            imgUsuario={postItem.imgUsuario}
+            nomeUsuario={postItem.nomeUsuario}
+            userPost={postItem.userPost}
+            imgCurtiu={postItem.imgCurtiu}
+            nomeCurtiu={postItem.nomeCurtiu}
+            qtdCurtiu={postItem.qtdCurtiu}
+          />
+        );
+      })}
+    </div>
+  );
+}
